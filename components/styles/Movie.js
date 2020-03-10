ec90845fa843d5b7ce4d8d109c2a17f9fd6ffa6b
AbdullhRtima <abdullahaburtima@gmail.com>
2020-03-10 19:57:36 +0200

    responsive

--- components/styles/Movie.js
+++ components/styles/Movie.js
@@ -5,6 +5,7 @@ const MovieDetials = styled.div`
     position:relative;
     z-index:1;
     display:flex;
+    flex-direction:row;
     justify-content:space-between;
     .movie_details{
         color:#ffff;
@@ -39,11 +40,28 @@ const MovieDetials = styled.div`
         color:#01d277;
         margin-right:1rem;
     }
+@media screen and (max-width:800px) {
+    margin-top:10rem;
+    position:relative;
+    z-index:1;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+}
 `
 const Container = styled.div`
     .youtube{
         margin-left:20rem;
         margin-top:2rem;
     }
+ @media screen and (max-width:800px) {
+    .youtube{
+        margin-left:20rem;
+        margin-top:2rem;
+        width:40rem;
+        margin-left:-.8rem;
+   }
+}
+
 `
 export {MovieDetials ,Container}
