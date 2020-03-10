2733bb9072ad7daefb8dd7d3261ee1196e8d4125
AbdullhRtima <abdullahaburtima@gmail.com>
2020-03-10 23:07:04 +0200

    nprogress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import Page from '../components/Page'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import Head from 'next/head'
+
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`)
+  NProgress.start()
+})
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
+
 return(
-            <Page>
-                <Component {...pageProps} />
-            </Page>    
+        <Page>
+             <Head>
+              {/* Import CSS for nprogress */}
+              <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+            </Head>
+            <Component {...pageProps} />
+        </Page>    
     ) 
   }
 
